241c18b70fad1e8241b3ddc2398d790a9f0eac5f
Fikret Yilmaz <yilmz.fikret@gmail.com>
2021-09-14 20:11:18 +0200

    notepadapp last update before final

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ selectSection.className = 'text-input';
 tagAreaEl.appendChild(selectSection);
 const button = document.querySelector('#save-data');
 //GET THE JSON
-async function fetchData(filteredChar) {
+async function fetchData() {
   try {
     const key = 'api_key=1bbe5e8200e247b02e555f96904c5de3';
     const url = `https://api.themoviedb.org/3`;
@@ -15,7 +15,7 @@ async function fetchData(filteredChar) {
       const data = await response.json();
       const modifyData = data.genres.map((data) => {
         if (data.name === 'Action') {
-          data.name = 'None';
+          data.name = '--Select A Tag--';
         }
         if (data.name === 'Crime') {
           data.name = 'Important';
@@ -43,7 +43,10 @@ async function fetchData(filteredChar) {
       throw 'HTTP ERROR';
     }
   } catch (err) {
-    console.log(err);
+    alert(
+      'Can not get the data please refresh page or control your internet connection',
+      err,
+    );
   }
 }
 //CREATE THE NOTES
@@ -51,13 +54,13 @@ function saveData(data) {
   try {
     const getTextContent = document.querySelector('#id-content');
     const getCreateTag = document.querySelector('#id-title');
-    if (data === 'None' && getCreateTag.value === '') {
+    if (data === '--Select A Tag--' && getCreateTag.value === '') {
       alert('Please Choose a Tag or Create a New One');
-    } else if (data !== 'None' && getCreateTag.value !== '') {
+    } else if (data !== '--Select A Tag--' && getCreateTag.value !== '') {
       alert(
         `Please Select only one. You Couldn't Choose a Tag and Create a New One At The Same Time`,
       );
-    } else if (data !== 'None' || getCreateTag.value !== '') {
+    } else if (data !== '--Select A Tag--' || getCreateTag.value !== '') {
       const noteSection = document.querySelector('.note-section');
       const noteContainer = document.createElement('article');
       noteContainer.className = 'note-container';
@@ -79,17 +82,17 @@ function saveData(data) {
       content.appendChild(contentText);
       deleteNote(noteContainer);
       //searchNote(noteContainer);
-      if (data !== 'None') {
+      if (data !== '--Select A Tag--') {
         title.textContent = data;
         contentText.textContent = getTextContent.value;
         showCategory(title.textContent, noteContainer);
-        searchNote(title.textContent, contentText.textContent, noteContainer);
+        searchNote(title.textContent, contentText.textContent);
       }
       if (getCreateTag.value !== '') {
         title.textContent = getCreateTag.value;
         contentText.textContent = getTextContent.value;
         showCategory(title.textContent, noteContainer);
-        searchNote(title.textContent, contentText.textContent, noteContainer);
+        searchNote(title.textContent, contentText.textContent);
       }
     }
   } catch (error) {
@@ -121,32 +124,33 @@ function showCategory(title, noteContainer) {
   const categorySection = document.querySelector('.category-section');
   const categoryButton = document.createElement('button');
   categoryButton.className = 'category dom';
+  const buttonAll = document.querySelector('#all');
+  buttonAll.addEventListener('click', () => {
+    noteContainer.className = 'note-container';
+    noteContainer.classList.remove('hide');
+  });
 
-  // const noteContainer = document.querySelector('.note-container');
   if (categoryList.includes(title)) {
   } else {
     categoryList.push(title);
     categoryList.map(() => {
+      //add button to the html category section
       categoryButton.textContent = title;
       categorySection.appendChild(categoryButton);
     });
   }
-  const buttonList = document.querySelectorAll('.dom');
-  console.log(buttonList);
-  buttonList.forEach((button) => {
-    button.addEventListener('click', () => {
-      noteContainer.classList.remove('hide');
-      noteContainer.className = 'note-container';
-    });
+  const arrCategoryButton = [categoryButton.innerHTML];
+  categoryButton.addEventListener('click', () => {
+    matchDataArray(arrCategoryButton);
   });
 }
 //DELETE THE NOTE
 function deleteNote(noteContainer) {
   const deleteButton = document.createElement('button');
-  deleteButton.style.width = '60px';
-  deleteButton.style.height = '60px';
   deleteButton.style.margin = 'auto';
   deleteButton.style.background = 'none';
+  deleteButton.className = 'category';
+  deleteButton.title = 'delete';
   const buttonIcon = document.createElement('img');
   buttonIcon.src = '../img/deleteIcon.svg';
   deleteButton.appendChild(buttonIcon);
@@ -157,34 +161,30 @@ function deleteNote(noteContainer) {
 }
 //SEARCH THE NOTE
 let characterList = [];
-function searchNote(title, contentText) {
+function searchNote(title) {
   const searchBar = document.getElementById('search-bar');
   if (characterList.includes(title)) {
-  }
-  if (characterList.includes(contentText)) {
   } else {
-    characterList.push(contentText);
     characterList.push(title);
   }
 
   searchBar.addEventListener('keyup', (e) => {
-    //e.preventDefault;
-    const searchString = e.target.value.toLowerCase();
+    e.preventDefault;
+    const searchString = e.target.value.toLowerCase().trim();
     const filteredChar = characterList.filter((character) => {
       return character.toLowerCase().trim().includes(searchString);
     });
-    const title2 = document.querySelectorAll('.title');
-    title2.forEach((data) => {
-      filteredChar.forEach((filter) => {
-        if (data.innerHTML === filter) {
-          data.parentElement.parentElement.classList.remove('hide'); //to get parent element
-          data.parentElement.parentElement.className = 'note-container'; //after get the parent element to fix class name
-        } else {
-          data.parentElement.parentElement.className = 'hide';
-        }
-      });
-    });
+    matchDataArray(filteredChar);
   });
 }
-
+function matchDataArray(filteredChar1) {
+  const title2 = document.querySelectorAll('.title');
+  title2.forEach((data) => {
+    if (filteredChar1.indexOf(data.innerHTML) >= 0) {
+      data.parentElement.parentElement.className = 'note-container';
+    } else {
+      data.parentElement.parentElement.className = 'hide';
+    }
+  });
+}
 fetchData();
